57bcd8125d34529cdf6cab1e095a9e9ec1ca2092
Beno04 <luluvic59@gmail.com>
2024-12-04 11:22:17 +0100

    changement fichier

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Barre de navigation -->
     <nav class="menu">
       <ul>
-        <li class="titre-marieteam"><b>MarieTeam</b></li>
+        <li class="titre-marieteam" ><a href="a_propos.html"><b>MarieTeam</b></a></li>
         <div class="nav-buttons">
           <li><a href="reserver.html">Réserver</a></li>
           <li><a class="active" href="a_propos.html">À propos</a></li>
@@ -21,7 +21,7 @@
     <!-- Section principale de bienvenue -->
     <br><br><br><br>
     <section class="intro">
-      <h1>Bienvenue chez <span class="highlight">MarieTeam</span></h1>
+      <h1>Bienvenue chez <span class="titre1">MarieTeam</span></h1>
       <h2>Votre Transport Maritime de Confiance</h2>
       <p>
         MarieTeam est le leader du transport maritime sur le littoral français, 
@@ -31,9 +31,12 @@
       </p>
     </section>
 
+    <!-- Ligne de séparation -->
+    <hr class="separator" />
+
     <!-- Section de réservation -->
     <section class="reservation">
-      <h1>Réservation en Ligne <span class="highlight">Simplifiée</span></h1>
+      <h1>Réservation en Ligne <span class="titre2">Simplifiée</span></h1>
       <p>
         Notre interface de réservation en ligne vous permet de planifier vos traversées en toute simplicité. 
         Consultez les horaires, découvrez nos tarifs et réservez votre place en quelques clics. 
