8c25a56580a7597ba453dbd2819a04fa500317e9
Axel <103052424+Axelvrdn@users.noreply.github.com>
2025-02-24 11:52:05 +0100

    Update Script.php

--- Fonctions/Script.php
+++ Fonctions/Script.php
@@ -1,6 +1,6 @@
 <?php
 
-// Récupere les secteurs qui ont des traverser après la date du jour
+// Récupere les secteurs qui ont des traversers après la date du jour
 $sql = "SELECT nom_secteur 
 FROM `secteur` 
 INNER JOIN liaison ON liaison.id_secteur = secteur.id_secteur
@@ -9,6 +9,23 @@ WHERE traversée.date_travers > CURRENT_DATE
 GROUP BY secteur.nom_secteur"
 
 
+//Récupere les descriptions des traversers après la date du jour par rapport au secteurs choisis
+$sql = "SELECT  traversée.desc_travers
+FROM `liaison` 
+INNER JOIN traversée ON liaison.id_travers = traversée.id_travers
+WHERE id_secteur = 1 AND traversée.date_travers > CURRENT_DATE
+GROUP BY traversée.desc_travers"
 
-$sql = ""
 
+//Récupere les dates des traversers après la date du jour par rapport a la description des traversers choisis
+$sql = "SELECT  traversée.date_travers
+FROM `liaison` 
+INNER JOIN traversée ON liaison.id_travers = traversée.id_travers
+WHERE traversée.desc_travers = 'Quiberon-Le Palais' AND traversée.date_travers > CURRENT_DATE
+GROUP BY traversée.date_travers"
+
+
+$sql = "SELECT id_travers, heure_travers, bateau.nom_bateau , contenir.capac_bateau_pass - COUNT()
+FROM `traversée`
+INNER JOIN bateau ON traversée.id_bateau = bateau.id_bateau
+WHERE heure_travers = '07:45:00'"
