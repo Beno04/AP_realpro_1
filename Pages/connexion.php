9f3638737b8c55a4964f900ec686957912efc2f7
Lucas <120470521+Beno04@users.noreply.github.com>
2025-02-27 09:16:34 +0100

    maj menu

--- Pages/connexion.php
+++ Pages/connexion.php
@@ -17,9 +17,20 @@
       <ul>
         <li class="titre-marieteam"><a href="index.php"><b>MarieTeam</b></a></li>
         <div class="nav-buttons">
-          <li><a href="reserver.php">Réserver</a></li>
-          <li><a href="index.php">À propos</a></li>
-          <li><a href="connexion.php"><b class="connexion-btn">Connexion</b></a></li>
+
+        <?php if (isset($prenom) && isset($nom)): ?>
+            <li><a href="reserver.php">Réserver</a></li>
+          <?php else: ?>
+            <li><a href="connexion.php">Réserver</a></li>
+          <?php endif; ?>
+
+          <li><a class="active" href="index.php">À propos</a></li>
+
+          <?php if (isset($prenom) && isset($nom)): ?>
+            <li><a href="profile.php"><b class="connexion-btn"><?php echo $nom . ' ' . $prenom; ?></b></a></li>
+          <?php else: ?>
+            <li><a href="connexion.php"><b class="connexion-btn">Connexion</b></a></li>
+          <?php endif; ?>
         </div>
       </ul>
     </nav>
--- Pages/reserver.php
+++ Pages/reserver.php
@@ -6,16 +6,28 @@
     <title>MarieTeam</title>
 </head>
 <body>
+    
     <!-- Barre de navigation -->
     <nav class="menu">
-        <ul>
-            <li class="titre-marieteam"><a href="index.php"><b>MarieTeam</b></a></li>
-            <div class="nav-buttons">
-                <li><a class="active" href="reserver.php">Réserver</a></li>
-                <li><a href="index.php">À propos</a></li>
-                <li><a href="connexion.php"><b class="connexion-btn">Connexion</b></a></li>
-            </div>
-        </ul>
+      <ul>
+        <li class="titre-marieteam"><a href="index.php"><b>MarieTeam</b></a></li>
+        <div class="nav-buttons">
+
+        <?php if (isset($prenom) && isset($nom)): ?>
+            <li><a href="reserver.php">Réserver</a></li>
+          <?php else: ?>
+            <li><a href="connexion.php">Réserver</a></li>
+          <?php endif; ?>
+
+          <li><a class="active" href="index.php">À propos</a></li>
+
+          <?php if (isset($prenom) && isset($nom)): ?>
+            <li><a href="profile.php"><b class="connexion-btn"><?php echo $nom . ' ' . $prenom; ?></b></a></li>
+          <?php else: ?>
+            <li><a href="connexion.php"><b class="connexion-btn">Connexion</b></a></li>
+          <?php endif; ?>
+        </div>
+      </ul>
     </nav>
 
     <?php
